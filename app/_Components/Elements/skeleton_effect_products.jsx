d914531765657915ea9b52f6982f78cc242fa50d
Nasser <nassergme99@gmail.com>
2024-08-03 18:04:45 +0000

    Change Width 2

--- app/_Components/Elements/skeleton_effect_products.jsx
+++ app/_Components/Elements/skeleton_effect_products.jsx
@@ -1,7 +1,7 @@
 // components/SkeletonCard.js
 export default function SkeletonCard() {
   return (
-    <div className="cursor-pointer flex flex-col bg-gray-50 sm:w-full sm:mx-0 w-[70%] h-[250px] mx-auto md:h-[245px] lg:h-[230px] rounded-lg shadow-md animate-pulse">
+    <div className="cursor-pointer flex flex-col bg-gray-50 sm:w-full sm:mx-0 w-[90%] h-[250px] mx-auto md:h-[245px] lg:h-[230px] rounded-lg shadow-md animate-pulse">
       <div id="img_container" className="h-[60%] w-[100%] bg-gray-200"></div>
       <div id="content" className="mt-auto pt-5 pb-2 px-2">
         <div className="h-4 bg-gray-200 rounded w-3/4 mb-2"></div>
